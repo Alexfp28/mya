f614e3988185d7283030659075110c4a7b783286
Alexfp28 <alexlopezdelafuente@gmail.com>
2024-11-30 18:28:33 +0100

    Prueba al mover imagenes de sitio

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 import '@/assets/login.css';
 export default {
   data() {
--- src/components/pages/SecondPageComponent.vue
+++ src/components/pages/SecondPageComponent.vue
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 import '@/assets/page.css'
 export default {
   data() {
